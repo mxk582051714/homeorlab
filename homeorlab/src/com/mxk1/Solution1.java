4b7ee9ae194508a793a354fbf0d60a4adfb184f7
Robin.m <meng_xiang__kai@163.com>
2018-04-05 13:29:50 +0800

    实现了冒泡排序算法

--- homeorlab/src/com/mxk1/Solution1.java
+++ homeorlab/src/com/mxk1/Solution1.java
@@ -1,10 +1,37 @@
 package com.mxk1;
 
+import java.util.Arrays;
+
 public class Solution1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int [] arr= {5,4,6,3,2};
+		function1(arr);
+		System.out.println(Arrays.toString(arr));
 	}
+	/**
+	 * function1：实现冒泡排序
+	 */
+	public static void function1(int [] arr) {
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=arr.length-1;j>=i+1;j--) {
+				if(arr[j-1]>arr[j])
+					swap(arr,j-1,j);
+			}
+		}
+	}
+	
+	
+	/**
+	 * swap:交换方法
+	 */
+	public static  void swap(int [] arr,int a,int b) {
+		int temp=arr[b];
+		arr[b]=arr[a];
+		arr[a]=temp;
+	}
+	
+	
 
 }
